4a9266fbd73ede35f36bf12a88adef1a6c4a8cef
Saulo Costa <saulomlcosta10@gmail.com>
2021-11-02 15:50:02 -0300

    Config the routes and controller

---
+++ laravelCrud/app/Http/Controllers/tasksController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class tasksController extends Controller
+{
+    public function list() {
+
+    }
+    public function create() {
+
+    }
+    public function createAction() {
+
+    }
+    public function edit() {
+
+    }
+    public function editAction() {
+
+    }
+    public function delete() {
+
+    }
+    public function done() {
+
+    }
+}
--- laravelCrud/routes/web.php
+++ laravelCrud/routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::prefix('/tasks')->group(function(){
+    Route::get('/', 'tasksController@list');
+
+    Route::get('/create', 'tasksController@create');
+    Route::post('/create', 'tasksController@createAction');
+
+    Route::get('/edit{id}', 'tasksController@edit');
+    Route::post('/edit{id}', 'tasksController@editAction');
+
+    Route::get('/delete{id}', 'tasksController@delete');
+
+    Route::get('/solved{id}', 'tasksController@done');
 });
