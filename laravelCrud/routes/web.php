92b83cbcd4156a0743f592e22f53f8afec92731b
Saulo Costa <saulomlcosta10@gmail.com>
2021-11-02 15:38:22 -0300

    Just Creating the Project

--- laravelCrud/routes/web.php
+++ laravelCrud/routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::resource('/ajaxposts', [PostAjaxController::class, 'index']);
-
