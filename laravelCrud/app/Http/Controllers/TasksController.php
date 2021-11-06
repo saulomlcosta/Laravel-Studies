7d8ee27e057a61fa36ae90a73f3125dacbda34b0
Saulo Costa <saulomlcosta10@gmail.com>
2021-11-05 23:21:08 -0300

    Creating the Edit Method

--- laravelCrud/app/Http/Controllers/TasksController.php
+++ laravelCrud/app/Http/Controllers/TasksController.php
@@ -32,13 +32,42 @@ class TasksController extends Controller
             return redirect('tasks/create')->with('warning', 'You need to give me a task first!');
         }
     }
-    public function edit() {
-        return view('tasks.edit');
-    }
-    public function editAction() {
+    public function edit($id) {
+        $data = DB::select('SELECT * FROM tasks WHERE id = :id', [
+            'id' => $id
+        ]);
 
+        if(count($data) > 0) {
+            return view('tasks.edit', [
+                'data' => $data[0]
+            ]);
+        } else {
+            return redirect('tasks');
+        }
     }
-    public function delete(Request $request) {
+    public function editAction(Request $request, $id) {
+        if($request->filled('title')) {
+            $title = $request->input('title');
+
+            $data = DB::select('SELECT * FROM tasks WHERE id = :id', [
+                'id' => $id
+            ]);
+
+            if(count($data) > 0) {
+                DB::update('UPDATE tasks SET title = :title WHERE id = :id', [
+                'id' => $id,
+                'title' => $title
+            ]);
+
+            return redirect('tasks');
+
+            } else {
+                return redirect('tasks');
+            }
+        }
+    }
+
+    public function delete() {
 
     }
     public function done() {
--- laravelCrud/resources/views/tasks/edit.blade.php
+++ laravelCrud/resources/views/tasks/edit.blade.php
@@ -4,4 +4,22 @@
 
 @section('content')
     <h1>Edit</h1>
+
+    <form method="POST">
+        @csrf
+
+        @if (session('warning'))
+            <div style="font-weight: bold; text-align:center; border:3px solid #FF0000; padding: 5px; width:25%">
+                {{ session('warning') }}
+            </div>
+        @endif
+
+        <label>
+            Title:<br/>
+            <input type="text" name="title" value="{{ $data->title }}">
+        </label>
+
+        <input type="submit" value="Save"><br>
+        <a href="/tasks">Return to list</a>
+    </form>
 @endsection
