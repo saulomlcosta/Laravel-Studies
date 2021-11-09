189b8ae8bcbcd82fcd0c065179feef004c3e5479
Saulo Costa <saulomlcosta10@gmail.com>
2021-11-09 00:59:10 -0300

    Fix

--- laravelCrud/app/Http/Controllers/Auth/LoginController.php
+++ laravelCrud/app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/tasks';
 
     /**
      * Create a new controller instance.
