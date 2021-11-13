61a2a96d95d9843f92f81583125e27b64b7581e5
Saulo Costa <saulomlcosta10@gmail.com>
2021-11-13 17:49:01 -0300

    Creating Todo Routes and the first methods at Controller

---
+++ laravelApi/app/Http/Controllers/ApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class apiController extends Controller
+{
+    public function createTodo() {
+
+    }
+    public function readAllTodo() {
+
+    }
+    public function readTodo() {
+
+    }
+    public function updateTodo() {
+
+    }
+    public function deleteTodo() {
+
+    }
+}
--- laravelApi/routes/api.php
+++ laravelApi/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,15 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/ping', function(){
     return [
         'pong' => true,
-        'Test' => [
-            'Atributtes' => 'Strength'
-        ]
     ];
 });
+
+Route::post('/todo', [ApiController::class, 'createTodo']);
+Route::get('/todos', [ApiController::class, 'readAllTodo']);
+Route::get('/todo/{id}', [ApiController::class, 'readTodo']);
+Route::put('/todo/{id}', [ApiController::class, 'updateTodo']);
+Route::delete('todo/{id}', [ApiController::class, 'deleteTodo']);
