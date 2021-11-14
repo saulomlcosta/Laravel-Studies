d161cb99b84e5c03d35dd9a38fa8beb767f9baa6
Saulo Costa <saulomlcosta10@gmail.com>
2021-11-13 22:35:45 -0300

    The first method created and useful

--- laravelApi/app/Http/Controllers/ApiController.php
+++ laravelApi/app/Http/Controllers/ApiController.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Validator;
+use App\Models\Todo;
+
 use Illuminate\Http\Request;
 
 class apiController extends Controller
 {
-    public function createTodo() {
+    public function createTodo(Request $request) {
+        $array = ['error' => ''];
 
+        $rules = [
+            'title' => 'required|min:3'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()) {
+            $array['error'] = $validator->errors();
+
+            return $array;
+        }
+
+        $title = $request->input('title');
+
+        $todo = new Todo();
+        $todo->title = $title;
+        $todo->save();
+
+        return $array;
     }
     public function readAllTodo() {
 
