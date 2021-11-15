f25c01ae6d8edb0e155a745d7a6d9969a9b39c0c
Saulo Costa <saulomlcosta10@gmail.com>
2021-11-15 17:15:25 -0300

    Layout, Bootstrap view, Modal and Routes 2

--- laravelAjaxCrud/routes/web.php
+++ laravelAjaxCrud/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('students', [StudentController::class, 'index']);
 
 /*
 |--------------------------------------------------------------------------
